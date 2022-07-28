a81e6b5590400a65e2f4f0abb056fae51b9daa1e
1998YUKI0331 <dkenl0110@dongguk.edu>
2022-07-29 03:55:48 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { useState, useRef } from "react";
+
+const ffmpeg = createFFmpeg({
+  log: true,
+  corePath: "https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js",
+});
 
 function App() {
+  const videoRef = useRef();
+  const [file, setFile] = useState();
+  const [previewSrc, setPreviewSrc] = useState("");
+
+  const handleFileSelected = async (e) => {
+    setFile(() => e.target.files[0]);
+    doTranscode(e.target.files[0]);
+  };
+
+  const doTranscode = async (file) => {
+    console.log("Loading ffmpeg-core.js");
+    await ffmpeg.load();
+
+    console.log("Start transcoding");
+    ffmpeg.FS(
+      "writeFile",
+      file.name,
+      await fetchFile(URL.createObjectURL(file))
+    );
+    await ffmpeg.run("-i", file.name, "-r", "60", "test.mp4");
+
+    console.log("Complete transcoding");
+
+    const mp4File = ffmpeg.FS("readFile", "test.mp4");
+    const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+
+    setPreviewSrc(URL.createObjectURL(mp4Blob));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" accept="video/*" onChange={handleFileSelected} />
+      <video
+        ref={videoRef}
+        width="100%"
+        height="100%"
+        controls
+        preload="metadata"
+        src={previewSrc}
+      />
     </div>
   );
 }
